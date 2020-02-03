bc4b2d7c7ca92db74580ff922e03754eeda5a265
RyanJeong <wat6988@gmail.com>
2020-02-04 03:06:02 +0900

    Updated on Feb 04 2020

---
+++ Tetris/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define START_GAME          1
+#define PRINT_GAME_MANUAL   2
+#define PRINT_GAME_RESULT   3
+#define EXIT_GAME           (-1)
+
+int getChoise(void);
+
+#endif
---
+++ Tetris/print.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "print.h"
+
+void printManual(void)
+{
+
+    return;
+}
+
+void printRanking(void)
+{
+	FILE    *fp;
+    int     c;
+
+	system("clear");
+    fp = NULL;
+}
