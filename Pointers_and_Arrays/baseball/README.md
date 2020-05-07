c27842a0b4a5751f09efaa2926168f0d731b2906
RyanJeong <wat6988@gmail.com>
2020-05-07 21:34:10 +0900

    Updated on May 7 2020

--- Pointers_and_Arrays/baseball/README.md
+++ Pointers_and_Arrays/baseball/README.md
@@ -194,3 +194,174 @@ Input 3 numbers without duplicates.
 Computer's random numbers are 5, 8, and 9
 You win!
 ```
+
+* Number Baseball Game 2nd version
+```C
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+#define BUFSIZE 100 
+
+enum {
+    CHECK_SIZE  = 10,   /*  중복 검사에 사용하는 배열의 갯수    */
+    INNINGS     = 9,    /*  야구 이닝(inning) 수                */
+    NUM_SIZE    = 3     /*  맞추어야 할 숫자의 갯수             */
+}; 
+
+/* void initGame(void);
+    프로그램이 실행되면 최초 한 번 실행되며, 해당 프로그램 실행에 필요한 과정을 처리하는 함수 */
+void    initGame(void);
+
+/* void initGame(void);
+    숫자 야구 게임의 동작이 구현되어 있는 함수 */
+void    playGame(void);
+
+/* void generateNumbers(int []);
+    3개의 랜덤 번호를 생성하는 함수 */
+void    generateNumbers(int []);
+
+/* int getInt(int *);
+    get next integer from input into *pn */
+int     getInt(int *);
+
+/* int getch(void);
+    get a (possibly pushed-back) character */
+int     getch(void);
+
+/* void ungetch(int);
+    push character back on input */
+void    ungetch(int);
+
+char    buf[BUFSIZE];   /* buffer for ungetch */
+int     bufp = 0;       /* next free position in buf */
+
+int main()
+{
+    initGame();
+    playGame();
+
+    return 0;
+}
+
+void initGame(void)
+{
+    srand(time(NULL));  /*  Initialize the seed.    */
+    printf("*** Number Baseball Game ***\n"
+           "You have 9 chances to find out the right answer with 10 numbers(0 - 9).\n");
+
+    return;
+}
+
+void playGame(void)
+{
+/* Variables
+    strike      : strike 갯수
+    ball        : ball 갯수
+    i, j, k     : 반복문 제어 변수
+    temp        : 플레이어가 입력한 숫자를 임시로 저장하는 변수
+    computer[]  : 컴퓨터가 생성한 정수
+    player[]    : 플레이어가 생성한 정수 */ 
+    int strike, ball, i, j, k, temp;
+    int computer[NUM_SIZE], player[NUM_SIZE];
+
+    generateNumbers(computer);
+    for (i = 1; i <= INNINGS && strike != NUM_SIZE; ++i) {
+        strike = ball = 0;
+        printf("Input 3 numbers without duplicates.\n");
+        for (j = 0; j < NUM_SIZE; ++j) {
+            getInt(player + j);
+        }
+        for (j = 0; j < NUM_SIZE; ++j) {
+            if (computer[j] == player[j]) {
+                ++strike;
+            } else if ((computer[j] == player[(j + 1)%NUM_SIZE]) || (computer[j] == player[(j + 2)%NUM_SIZE])) {
+                ++ball;
+            }
+        }
+        printf("[%d%s attempt] %d Strke%s, %d Ball%s\n", 
+               i, 
+               (i == 1) ? "st" : ((i == 2) ? "nd" : ((i == 3) ? "rd" : "th")), 
+               strike, 
+               (strike == 1) ? "" : "s", 
+               ball,
+               (ball == 1) ? "" : "s");
+    }
+    printf("Computer's random numbers are %d, %d, and %d\n"
+           "%s\n",
+           computer[0], 
+           computer[1], 
+           computer[2], 
+           (i > INNINGS) ? "You lose!" : "You win!");
+
+    return;
+}
+
+void generateNumbers(int c[])
+{
+/* Variables
+    i       : 반복문 제어 변수
+    num     : 난수 발생 후 생성되는 값을 저장하는 변수
+    check   : computer[] 값 중복을 방지하기 위해 사용하는 배열 */
+    int i, num;
+    int check[CHECK_SIZE];
+
+    /*  Init. the array */
+    for (i = 0; i < CHECK_SIZE; ++i) {
+        check[i] = 0;
+    }
+    for (i = 0; i < NUM_SIZE; ++i) {
+        while (check[num = (rand() % CHECK_SIZE)]) {
+            ;
+        }
+        check[num]  = 1;
+        c[i]        = num;
+    }
+
+    return;
+}
+
+int getch(void) 
+{    
+
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) 
+{    
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters \n");
+    } else {
+        buf[bufp++] = c;
+    }
+
+    return;
+}
+int getInt(int *pn)
+{    
+    int c, sign;
+    
+    while (isspace(c = getch())) { /* skip white space */
+        ;
+    }
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c); /* it is not a number */
+
+        return 0;
+    }
+    sign = (c == '-') ? (-1) : 1;
+    if (c == '+' || c == '-') {
+        c = getch();
+    }
+    for (*pn = 0; isdigit(c); c = getch()) {
+        *pn = 10 * *pn + (c - '0');
+    }
+    *pn *= sign;
+    if (c != EOF) {
+        ungetch(c);
+    }
+
+    return c;
+}
+```
---
+++ Pointers_and_Arrays/hangman/main_alt.c
@@ -0,0 +1,177 @@
+/*
+ *  Keywords in C Programming
+ *  -----------------------------
+ *  auto        break       case        char
+ *  const       continue    default     do
+ *  double      else        enum        extern
+ *  float       for         goto        if
+ *  int         long        register    return
+ *  short       signed      sizeof      static
+ *  struct      switch      typedef     union
+ *  unsigned    void        volatile    while
+ */
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h> /*  for functions related with string   */
+#include <time.h>   /*  for random number generation        */
+#include <ctype.h>
+
+#define TITLE       "*** Hangman ***"
+#define BLANK       "        "
+#define GALLOWS     "    +---"
+#define HEAD        "    O   "
+#define UPPER       "   /|\\  "
+#define BODY        "    |   "
+#define LOWER       "   / \\  "
+
+enum {
+    ATTEMPTS    = 10,
+    STATE_INIT  = 0,
+    MAX_SIZE    = 32
+};
+
+/*
+ *  int prepareGame(char [], char []);
+ *  ----------------
+ *  Prepare the game
+ *  return the target word's length
+ */
+int prepareGame(char [], char []);
+
+/*
+ *  int playGame(char **, char **, int);
+ *  ----------------
+ *  Implement the rules of hangman game
+ *  Return number of misses guesses
+ */
+int playGame(char [], char [], int);
+
+/*
+ *  void drawHangman(int);
+ *  ----------------
+ *  Draw part of the hangman when the players guess misses
+ */
+void drawHangman(int);
+
+int main(void)
+{
+/*
+ *  word: 맞추어야 하는 단어를 저장하는 변수
+ *  form: word의 글자 수만큼 '*' 문자를 저장하는 변수
+ *  len : word의 길이를 저장하는 변수
+ */
+    char    word[MAX_SIZE], form[MAX_SIZE];
+    int     len;
+
+    if ((len = prepareGame(word, form)) <= 0) {
+        printf("An occurred error while preparing the game.\n");
+    
+        return 1;
+    }
+    printf("Answer: %s\n"
+           "You %s!\n", 
+           word, 
+           (playGame(word, form, len) < ATTEMPTS) ? "win!" : "lose!");
+
+    return 0;
+}
+
+int prepareGame(char word[], char form[])
+{
+/*
+ *  words   : C언어의 keyword들을 보관하는 변수 
+ *  index   : keyword들 중 하나를 가리키기 위해 사용하는 인덱스
+ *  len     : 특정 keyword의 단어 길이
+ */
+    int         index, len;
+    static char *words[] = {
+        "auto", "beark", "case", "char",
+        "const", "continue", "default", "do",
+        "double", "else", "enum", "extern",
+        "float", "for", "goto", "if", 
+        "int", "long", "register", "return", 
+        "short", "signed", "sizeof", "static", 
+        "struct", "switch", "typedef", "union", 
+        "unsigned", "void", "volatile", "while"
+    };
+
+    drawHangman(STATE_INIT);
+    srand(time(NULL));
+    index   = (rand() % (sizeof(words) / sizeof(char *)));
+    len     = strlen(*(words + index));
+    strcpy(word, (words[index]));
+    memset(form, '*', len);
+    form[len] = '\0';
+    
+    return len;
+}
+
+int playGame(char word[], char form[], int len)
+{
+/*
+ *  misses      : 단어 추측에 실패한 횟수
+ *  i           : 반복문 제어 변수
+ *  c           : getchar() 결과를 보관하는 변수
+ *  check       : 사용자로부터 입력받은 문자가 정답 내 포함 여부를 저장하는 변수
+ *  remains     : 정답까지 남은 문자 갯수
+ *  listOfChar  : 단어 추측에 실패한 문자 목록
+ */
+    int     misses, i, c, check, remains;
+    char    listOfChar[ATTEMPTS];
+
+    misses  = 0;
+    remains = len;
+    while ((misses < ATTEMPTS) && remains) {
+        printf("Hint: %s, Input a character.\n"
+               "Misses: %d / %d\n",
+               form, misses, ATTEMPTS);
+        for (i = 0; i < misses; ++i) {
+            printf("%c ", listOfChar[i]);
+        }
+        putchar('\n');
+        while ((c = getchar()) != EOF) {
+            if (!isspace(c)) {
+                if (isalpha(c)) {
+                    c = (isupper(c)) ? (c += ('a' - 'A')) : c;
+                    for (check = i = 0; i < len; ++i) {
+                        if (word[i] == c && form[i] == '*') {
+                            form[i] = c;
+                            ++check;
+                            --remains;
+                        }
+                    }
+                    /*  Miss    */
+                    if (!check) {
+                        listOfChar[misses++] = c;
+                    }
+                    drawHangman(misses);
+                    break;
+                } else {
+                    printf("Wrong input!\n");
+                }
+            }
+        }
+    }
+
+    return misses;
+}
+
+void drawHangman(int misses)
+{
+    system("clear");
+    printf("%s\n"
+           "%s%c\n"
+           "%s%c\n"
+           "%s%c\n"
+           "%s%c\n"
+           "%s%c\n",
+           "*** Hangman ***",
+           (misses > 5) ? GALLOWS : BLANK, (misses > 4) ? '+' : ' ',
+           (misses > 6) ? HEAD : BLANK,    (misses > 3) ? '|' : ' ',
+           (misses > 7) ? UPPER : BLANK,   (misses > 2) ? '|' : ' ',
+           (misses > 8) ? BODY : BLANK,    (misses > 1) ? '|' : ' ',
+           (misses > 9) ? LOWER : BLANK,   (misses > 0) ? '|' : ' ');
+
+    return;
+}
