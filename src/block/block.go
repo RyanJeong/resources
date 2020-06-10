04dfae2a441a635ab39eb0c1c561e231d7a1f24a
RyanJeong <wat6988@gmail.com>
2020-06-10 21:33:43 +0900

    Updated on Jun 10 2020

--- src/block/block.go
+++ src/block/block.go
@@ -3,12 +3,16 @@ package block
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
+
+	"github.com/ryanjeong/go_blockchain/src/util"
 )
 
-type I_consensus interface {
-}
+const targetBits = 24
 
 type Block struct {
 	Timestamp     int64
@@ -22,10 +26,25 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
 func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.Blocks = append(bc.Blocks, newBlock)
+
+	/* Display the new block's infomation */
+	fmt.Printf("Timestamp: %d\n", newBlock.Timestamp)
+	fmt.Printf("Data: %s\n", newBlock.Data)
+	fmt.Printf("Prev. hash: %x\n", newBlock.PrevBlockHash)
+	fmt.Printf("Hash: %x\n", newBlock.Hash)
+	fmt.Println()
+	pow := NewProofOfWork(newBlock)
+	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+	fmt.Println()
 
 	return
 }
@@ -42,7 +61,7 @@ func (b *Block) SetHash() {
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
-	pow := consensus.NewProofOfWork(block)
+	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
 
 	block.Hash = hash[:]
@@ -59,4 +78,64 @@ func NewGenesisBlock() *Block {
 func NewBlockchain() *Blockchain {
 
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			util.IntToHex(pow.block.Timestamp),
+			util.IntToHex(int64(targetBits)),
+			util.IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	maxNonce := math.MaxInt64
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
 }
--- src/main.go
+++ src/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
 	"github.com/ryanjeong/go_blockchain/src/block"
-	"github.com/ryanjeong/go_blockchain/src/consensus"
 )
 
 func main() {
@@ -13,17 +9,6 @@ func main() {
 
 	bc.AddBlock("Send 1 BTC to Ivan")
 	bc.AddBlock("Send 2 more BTC to Ivan")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Timestamp: %d\n", block.Timestamp)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-		pow := consensus.NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
 
 	return
 }
