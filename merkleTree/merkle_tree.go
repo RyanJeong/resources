1fc24991d2abb9ccba4a10eb70a72ed52bdcf5e3
RyanJeong <wat6988@gmail.com>
2020-07-02 21:35:00 +0900

    Updated on Jul 2 2020

--- merkleTree/merkle_tree.go
+++ merkleTree/merkle_tree.go
@@ -1,20 +1,8 @@
-package main
+package merkleTree
 
 import (
 	"crypto/sha256"
 )
-
-// MerkleTree represent a Merkle tree
-type MerkleTree struct {
-	RootNode *MerkleNode
-}
-
-// MerkleNode represent a Merkle tree node
-type MerkleNode struct {
-	Left  *MerkleNode
-	Right *MerkleNode
-	Data  []byte
-}
 
 // NewMerkleTree creates a new Merkle tree from a sequence of data
 func NewMerkleTree(data [][]byte) *MerkleTree {
