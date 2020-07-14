b29c8452352c7e7964248359725a98b937941a07
RyanJeong <wat6988@gmail.com>
2020-07-14 22:16:11 +0900

    Updated on Jul 14 2020

--- tcp/client/client.go
+++ tcp/client/client.go
@@ -1,25 +1,100 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
-const ip = "192.168.0.26"
-const port = ":7654"
+func init() {
+	gob.Register(MyMsgBodyPing{})
+}
+
+type MyMsg struct {
+	Header MyMsgHeader
+	Body   interface{}
+}
+type MyMsgHeader struct {
+	MsgType string
+	Date    string
+}
+type MyMsgBodyPing struct {
+	Content string
+}
+
+type MyConnection struct {
+	send chan MyMsg
+
+	conn            net.Conn
+	enc             *gob.Encoder
+	codecBuffer     bytes.Buffer
+	codecBufferLock sync.Mutex
+}
+
+func (mc *MyConnection) SendMessage(msg MyMsg) error {
+	mc.codecBufferLock.Lock()
+	defer mc.codecBufferLock.Unlock()
+
+	mc.codecBuffer.Reset()
+
+	mc.enc.Encode(msg)
+
+	lengthBuf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(lengthBuf, uint32(mc.codecBuffer.Len()))
+
+	if _, err := mc.conn.Write(lengthBuf); nil != err {
+		log.Printf("failed to send msg length; err: %v", err)
+		return err
+	}
+
+	if _, err := mc.conn.Write(mc.codecBuffer.Bytes()); nil != err {
+		log.Printf("failed to send msg; err: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (mc *MyConnection) MyMsgSender() {
+	for {
+		select {
+		case msg := <-mc.send:
+			if err := mc.SendMessage(msg); nil != err {
+				log.Printf("failed to send message; err: %v", err)
+			}
+		}
+	}
+}
 
 func main() {
-	conn, err := net.Dial("tcp", ip+port)
+	conn, err := net.Dial("tcp", "server0:5032")
 	if nil != err {
 		log.Fatalf("failed to connect to server")
 	}
 
-	// do something ...
+	mc := MyConnection{
+		conn: conn,
+		send: make(chan MyMsg),
+	}
+	mc.enc = gob.NewEncoder(&mc.codecBuffer)
+
+	go mc.MyMsgSender()
 
 	for {
-		// heartbeat
-		conn.Write([]byte("ping"))
-		time.Sleep(time.Duration(1) * time.Second)
+		mc.send <- MyMsg{
+			Header: MyMsgHeader{
+				MsgType: "ping",
+				Date:    time.Now().UTC().Format(time.RFC3339),
+			},
+			Body: MyMsgBodyPing{
+				Content: "Hello! I'm alive!",
+			},
+		}
+
+		time.Sleep(time.Duration(3) * time.Second)
 	}
 }
